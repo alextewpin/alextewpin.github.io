aeba138c5b34223e5e781a69c64f912055112a2a
Alexander Tewpin <alex.tewpin@gmail.com>
2015-09-23 19:01:31 +0300

    basic stuff

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,49 @@
+var data
+
 var App = React.createClass({displayName: "App",
+	getInitialState: function () {
+		var feed = [];
+		for (category in this.props.data) {
+			this.props.data[category].forEach(function(item){
+				if (category == "mainProjects") {
+					item.type = "main"
+				}
+				item.date = new Date(item.date);
+				feed.push(item);
+			}, this);
+		}
+		feed.sort(function(a, b) {
+			return new Date(b.date) - new Date(a.date);
+		});
+	    return {
+	        feed: feed  
+	    };
+	},
 	render: function() {
 		return (
-			React.createElement("div", null, "OLOLO")
+			React.createElement("div", null, 
+				this.state.feed.map(function(item){
+					return React.createElement(Project, React.__spread({},  item))
+				}, this)
+			)
 		)
 	}
 })
 
-React.render(React.createElement(App), document.getElementById('react'));
+var Project = React.createClass({displayName: "Project",
+	render: function() {
+		var typeClass = "project project_type_" + this.props.type;
+		return (
+			React.createElement("div", {className: typeClass}, 
+				React.createElement("div", {className: "project__name"}, this.props.name), 
+				React.createElement("div", {className: "project__description"}, this.props.description), 
+				React.createElement("div", {className: "project__date"}, this.props.date.toString()), 
+				React.createElement("div", {className: "project__link"}, this.props.link)
+			)
+		)
+	}
+})
+
+$.get("data/data.json", function(data){
+	React.render(React.createElement(App, {data: data}), document.body);
+})
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,9 +1,49 @@
+var data
+
 var App = React.createClass({
+	getInitialState: function () {
+		var feed = [];
+		for (category in this.props.data) {
+			this.props.data[category].forEach(function(item){
+				if (category == "mainProjects") {
+					item.type = "main"
+				}
+				item.date = new Date(item.date);
+				feed.push(item);
+			}, this);
+		}
+		feed.sort(function(a, b) {
+			return new Date(b.date) - new Date(a.date);
+		});
+	    return {
+	        feed: feed  
+	    };
+	},
 	render: function() {
 		return (
-			<div>OLOLO</div>
+			<div>
+				{this.state.feed.map(function(item){
+					return <Project {...item} />
+				}, this)}
+			</div>
 		)
 	}
 })
 
-React.render(React.createElement(App), document.getElementById('react'));
+var Project = React.createClass({
+	render: function() {
+		var typeClass = "project project_type_" + this.props.type;
+		return (
+			<div className={typeClass}>
+				<div className="project__name">{this.props.name}</div>
+				<div className="project__description">{this.props.description}</div>
+				<div className="project__date">{this.props.date.toString()}</div>
+				<div className="project__link">{this.props.link}</div>
+			</div>
+		)
+	}
+})
+
+$.get("data/data.json", function(data){
+	React.render(<App data={data}/>, document.body);
+})
